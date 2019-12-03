72becfc9dd902f776bbfcd56795b59b77ce8573b
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-12-03 11:37:55 +0600

    css and js

---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+    <!-- Footer -->
+    <footer class="page-footer font-small blue">
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2018 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    </body>
+
+    </html>
