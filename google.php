dd64b950b676bee473fbb824ea030f5be846b99f
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-12-08 16:20:45 +0600

    update

--- google.php
+++ google.php
@@ -7,23 +7,19 @@ include('google-init.php');
 
 $login_button = '';
 
-//This $_GET["code"] variable value received after user has login into their Google Account redirct to PHP script then this variable value has been received
+
 if (isset($_GET["code"])) {
     //It will Attempt to exchange a code for an valid authentication token.
     $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
 
-    //This condition will check there is any error occur during geting authentication token. If there is no any error occur then it will execute if block of code/
+
     if (!isset($token['error'])) {
-        //Set the access token used for requests
         $google_client->setAccessToken($token['access_token']);
 
-        //Store "access_token" value in $_SESSION variable for future use.
         $_SESSION['access_token'] = $token['access_token'];
 
-        //Create Object of Google Service OAuth 2 class
         $google_service = new Google_Service_Oauth2($google_client);
 
-        //Get user profile data from google
         $data = $google_service->userinfo->get();
 
         //Below you can find Get profile data and store into $_SESSION variable
@@ -49,9 +45,7 @@ if (isset($_GET["code"])) {
     }
 }
 
-//This is for check user has login into system by using Google account, if User not login into system then it will execute if block of code and make code for display Login link for Login using Google account.
 if (!isset($_SESSION['access_token'])) {
-    //Create a URL to obtain user authorization
     $login_button = '<a href="' . $google_client->createAuthUrl() . '"><img src="sign-in-with-google.png" /></a>';
 }
 
@@ -71,7 +65,7 @@ if (!isset($_SESSION['access_token'])) {
 <body>
     <div class="container">
         <br />
-        <h2 align="center">PHP Login using Google Account</h2>
+        <h2>PHP Login using Google Account</h2>
         <br />
         <div class="panel panel-default">
             <?php
