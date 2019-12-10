2dd3a7cb1af54dd0520f3250089e8be97a3d9d80
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-12-10 18:08:06 +0600

    github and google

--- google.php
+++ google.php
@@ -5,8 +5,6 @@
 //Include Configuration File
 include('google-init.php');
 
-$login_button = '';
-
 
 if (isset($_GET["code"])) {
     //It will Attempt to exchange a code for an valid authentication token.
@@ -44,43 +42,29 @@ if (isset($_GET["code"])) {
         }
     }
 }
-
+$login_button = '';
 if (!isset($_SESSION['access_token'])) {
-    $login_button = '<a href="' . $google_client->createAuthUrl() . '"><img src="sign-in-with-google.png" /></a>';
+    $login_button = '<a class="btn btn-social-icon btn-google" href="' . $google_client->createAuthUrl() . '"><span class="fa fa-google"></span></a>';
 }
 
 ?>
-<html>
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>PHP Login using Google Account</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport' />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
-
-</head>
-
-<body>
-    <div class="container">
-        <br />
-        <h2>PHP Login using Google Account</h2>
-        <br />
-        <div class="panel panel-default">
-            <?php
-            if ($login_button == '') {
-                echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
-                echo '<img src="' . $_SESSION["user_image"] . '" class="img-responsive img-circle img-thumbnail" />';
-                echo '<h3><b>Name :</b> ' . $_SESSION['user_first_name'] . ' ' . $_SESSION['user_last_name'] . '</h3>';
-                echo '<h3><b>Email :</b> ' . $_SESSION['user_email_address'] . '</h3>';
-                echo '<h3><a href="logout.php">Logout</h3></div>';
-            } else {
-                echo '<div align="center">' . $login_button . '</div>';
-            }
-            ?>
-        </div>
+<?php include('inc/header.php'); ?>
+<div class="container">
+    <br />
+    <h2>PHP Login using Google Account</h2>
+    <br />
+    <div class="panel panel-default">
+        <?php
+        if ($login_button == '') {
+            echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
+            echo '<img src="' . $_SESSION["user_image"] . '" class="img-thumbnail rounded mx-auto d-block" heigh="900px" width="300px" />';
+            echo '<h3><b>Name :</b> ' . $_SESSION['user_first_name'] . ' ' . $_SESSION['user_last_name'] . '</h3>';
+            echo '<h3><b>Email :</b> ' . $_SESSION['user_email_address'] . '</h3>';
+            echo '<h3><a href="logout.php">Logout</h3></div>';
+        } else {
+            echo '<div align="center">' . $login_button . '</div>';
+        }
+        ?>
     </div>
-</body>
-
-</html>
+</div>
+<?php include('inc/footer.php'); ?>
